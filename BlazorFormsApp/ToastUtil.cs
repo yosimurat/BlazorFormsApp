b87dcc103dd642a8752ef7e73882419261a038ae
Toshiyuki Yoshimura <yoshimura@drytask.co.jp>
2023-06-14 17:00:09 +0900

    feat: Show notification test

---
+++ BlazorFormsApp/ToastUtil.cs
@@ -0,0 +1,19 @@
+﻿//using Microsoft.Windows.AppNotifications;
+//using Microsoft.Windows.AppNotifications.Builder;
+
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace BlazorFormsApp
+{
+    class ToastUtil
+    {
+        public static void Toast(string toastMsg, string toastTitle)
+        {
+            new ToastContentBuilder()
+                 .AddText(toastTitle)
+                 .AddText(toastMsg)
+                 .Show();
+
+        }
+    }
+}
